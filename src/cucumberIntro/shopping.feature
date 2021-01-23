d55b3b37c88332c49fbd9c68bf57e169636a0510
Humayun Ahmed Ashik <humayun_ruetcse13@yahoo.com>
2021-01-23 22:34:55 +0600

    examples concept in feature file

--- src/cucumberIntro/shopping.feature
+++ src/cucumberIntro/shopping.feature
@@ -2,17 +2,16 @@
 
 Feature: Check my basket application
 
-Scenario: No item selected in basket
+Scenario Outline: No item selected in basket
 Given I start the application
 Then I should see a selection of products
-When I browse "without" buying product
-Then my basket "should" be empty
-And I "cannot" buy an item
+When I browse "<Buying_Products>" buying product
+Then my basket "<Basket_status>" be empty
+And I "<Buy_status>" buy an item
 
 
-Scenario: No item selected in basket
-Given I start the application
-Then I should see a selection of products
-When I browse "with" buying product
-Then my basket "shouldnot" be empty
-And I "can" buy an item
+Examples:
+|Buying_Products|Basket_status|Buy_status|
+|without        |should       |cannot    |
+|with           |shouldnot    |can       |
+|xxx            |yyy          |zzz       |
--- target/cucmber-html-report/report.js
+++ target/cucmber-html-report/report.js
@@ -16,13 +16,101 @@ formatter.feature({
     }
   ]
 });
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 5,
   "name": "No item selected in basket",
   "description": "",
   "id": "check-my-basket-application;no-item-selected-in-basket",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 6,
+  "name": "I start the application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should see a selection of products",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "I browse \"\u003cBuying_Products\u003e\" buying product",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "my basket \"\u003cBasket_status\u003e\" be empty",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "I \"\u003cBuy_status\u003e\" buy an item",
+  "keyword": "And "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "check-my-basket-application;no-item-selected-in-basket;",
+  "rows": [
+    {
+      "cells": [
+        "Buying_Products",
+        "Basket_status",
+        "Buy_status"
+      ],
+      "line": 14,
+      "id": "check-my-basket-application;no-item-selected-in-basket;;1"
+    },
+    {
+      "cells": [
+        "without",
+        "should",
+        "cannot"
+      ],
+      "line": 15,
+      "id": "check-my-basket-application;no-item-selected-in-basket;;2"
+    },
+    {
+      "cells": [
+        "with",
+        "shouldnot",
+        "can"
+      ],
+      "line": 16,
+      "id": "check-my-basket-application;no-item-selected-in-basket;;3"
+    },
+    {
+      "cells": [
+        "xxx",
+        "yyy",
+        "zzz"
+      ],
+      "line": 17,
+      "id": "check-my-basket-application;no-item-selected-in-basket;;4"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "No item selected in basket",
+  "description": "",
+  "id": "check-my-basket-application;no-item-selected-in-basket;;2",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Smoke"
+    },
+    {
+      "line": 1,
+      "name": "@Sanity"
+    }
+  ]
 });
 formatter.step({
   "line": 6,
@@ -37,30 +125,39 @@ formatter.step({
 formatter.step({
   "line": 8,
   "name": "I browse \"without\" buying product",
+  "matchedColumns": [
+    0
+  ],
   "keyword": "When "
 });
 formatter.step({
   "line": 9,
   "name": "my basket \"should\" be empty",
+  "matchedColumns": [
+    1
+  ],
   "keyword": "Then "
 });
 formatter.step({
   "line": 10,
   "name": "I \"cannot\" buy an item",
+  "matchedColumns": [
+    2
+  ],
   "keyword": "And "
 });
 formatter.match({
   "location": "shoppingStep.I_start_the_application()"
 });
 formatter.result({
-  "duration": 100962200,
+  "duration": 114678500,
   "status": "passed"
 });
 formatter.match({
   "location": "shoppingStep.I_should_see_a_selection_of_products()"
 });
 formatter.result({
-  "duration": 52000,
+  "duration": 76300,
   "status": "passed"
 });
 formatter.match({
@@ -73,7 +170,7 @@ formatter.match({
   "location": "shoppingStep.I_browse_without_buying_product(String)"
 });
 formatter.result({
-  "duration": 2010000,
+  "duration": 2052000,
   "status": "passed"
 });
 formatter.match({
@@ -86,7 +183,7 @@ formatter.match({
   "location": "shoppingStep.my_basket_should_be_empty(String)"
 });
 formatter.result({
-  "duration": 297500,
+  "duration": 85100,
   "status": "passed"
 });
 formatter.match({
@@ -99,54 +196,73 @@ formatter.match({
   "location": "shoppingStep.I_cannot_buy_an_item(String)"
 });
 formatter.result({
-  "duration": 110000,
+  "duration": 125100,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 13,
+  "line": 16,
   "name": "No item selected in basket",
   "description": "",
-  "id": "check-my-basket-application;no-item-selected-in-basket",
+  "id": "check-my-basket-application;no-item-selected-in-basket;;3",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Smoke"
+    },
+    {
+      "line": 1,
+      "name": "@Sanity"
+    }
+  ]
 });
 formatter.step({
-  "line": 14,
+  "line": 6,
   "name": "I start the application",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 15,
+  "line": 7,
   "name": "I should see a selection of products",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 16,
+  "line": 8,
   "name": "I browse \"with\" buying product",
+  "matchedColumns": [
+    0
+  ],
   "keyword": "When "
 });
 formatter.step({
-  "line": 17,
+  "line": 9,
   "name": "my basket \"shouldnot\" be empty",
+  "matchedColumns": [
+    1
+  ],
   "keyword": "Then "
 });
 formatter.step({
-  "line": 18,
+  "line": 10,
   "name": "I \"can\" buy an item",
+  "matchedColumns": [
+    2
+  ],
   "keyword": "And "
 });
 formatter.match({
   "location": "shoppingStep.I_start_the_application()"
 });
 formatter.result({
-  "duration": 77000,
+  "duration": 88800,
   "status": "passed"
 });
 formatter.match({
   "location": "shoppingStep.I_should_see_a_selection_of_products()"
 });
 formatter.result({
-  "duration": 50700,
+  "duration": 66300,
   "status": "passed"
 });
 formatter.match({
@@ -159,7 +275,7 @@ formatter.match({
   "location": "shoppingStep.I_browse_without_buying_product(String)"
 });
 formatter.result({
-  "duration": 1233200,
+  "duration": 1873200,
   "status": "passed"
 });
 formatter.match({
@@ -172,7 +288,7 @@ formatter.match({
   "location": "shoppingStep.my_basket_should_be_empty(String)"
 });
 formatter.result({
-  "duration": 145500,
+  "duration": 459800,
   "status": "passed"
 });
 formatter.match({
@@ -185,7 +301,112 @@ formatter.match({
   "location": "shoppingStep.I_cannot_buy_an_item(String)"
 });
 formatter.result({
-  "duration": 100400,
+  "duration": 183800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "No item selected in basket",
+  "description": "",
+  "id": "check-my-basket-application;no-item-selected-in-basket;;4",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Smoke"
+    },
+    {
+      "line": 1,
+      "name": "@Sanity"
+    }
+  ]
+});
+formatter.step({
+  "line": 6,
+  "name": "I start the application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should see a selection of products",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "I browse \"xxx\" buying product",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "my basket \"yyy\" be empty",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "I \"zzz\" buy an item",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.match({
+  "location": "shoppingStep.I_start_the_application()"
+});
+formatter.result({
+  "duration": 86200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "shoppingStep.I_should_see_a_selection_of_products()"
+});
+formatter.result({
+  "duration": 58700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "xxx",
+      "offset": 10
+    }
+  ],
+  "location": "shoppingStep.I_browse_without_buying_product(String)"
+});
+formatter.result({
+  "duration": 95700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "yyy",
+      "offset": 11
+    }
+  ],
+  "location": "shoppingStep.my_basket_should_be_empty(String)"
+});
+formatter.result({
+  "duration": 132800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "zzz",
+      "offset": 3
+    }
+  ],
+  "location": "shoppingStep.I_cannot_buy_an_item(String)"
+});
+formatter.result({
+  "duration": 102600,
   "status": "passed"
 });
 });
